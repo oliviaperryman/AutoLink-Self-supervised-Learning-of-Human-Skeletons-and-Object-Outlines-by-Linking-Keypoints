883cca7e1a15d93a76021c4e73f9f995ebd4dc66
Olivia Perryman <oliviaperryman@gmail.com>
2023-05-14 03:43:38 -0700

    Add rotation of keypoints in encoder and use rotated image for masked image

--- models/encoder.py
+++ models/encoder.py
@@ -3,6 +3,9 @@ import torch.nn.functional as F
 from torch import nn
 import pytorch_lightning as pl
 
+import numpy as np
+from scipy.spatial.transform import Rotation as R
+
 
 def gen_grid2d(grid_size: int, left_end: float=-1, right_end: float=1) -> torch.Tensor:
     """
@@ -78,6 +81,7 @@ class Detector(nn.Module):
             # save memory in 3D, make grid only 16x16x16
             # TransposedBlock(256, 128),  # 32 
             nn.Conv2d(256, self.n_parts * self.output_size, kernel_size=3, padding=1),
+            torch.nn.Unflatten(1, (self.n_parts,self.output_size)),
         )
 
         # grid = gen_grid2d(self.output_size).reshape(1, 1, self.output_size ** 2, 2) 
@@ -91,6 +95,35 @@ class Detector(nn.Module):
         keypoints = self.coord * prob_map
         keypoints = keypoints.sum(dim=2)
         prob_map = prob_map.reshape(keypoints.shape[0], self.n_parts, self.output_size, self.output_size, self.output_size)
+
+        # rotate keypoints
+        # center keypoints around 0,0,0
+        # centered_keypoints = keypoints - self.output_size / 2
+        
+        # Change keypoints from (depth, height, width) to (width, height, depth)
+        perm = torch.LongTensor([2, 1, 0])
+        keypoints_xyz = keypoints[:, :, perm]
+
+        detached_keypoints = keypoints_xyz.detach().cpu().numpy()
+
+        # rotate around y axis
+        for batch in range(keypoints.shape[0]):
+            r = R.from_rotvec(input_dict["degrees"][batch].detach().cpu().numpy() * np.array([0, 0, 1]), degrees=True)
+            for kp in range(keypoints.shape[1]):
+                detached_keypoints[batch, kp] = r.apply(detached_keypoints[batch, kp])
+
+        keypoints_xyz_rotated = torch.tensor(detached_keypoints).to(input_dict['img'].device)
+
+        # move keypoints back to original position
+        # keypoints = centered_keypoints + self.output_size / 2
+
+        # Change keypoints from (width, height, depth) to (width, height)
+        keypoints_xy_rotated = keypoints_xyz_rotated[:,:,:2]
+
+        # Change keypoints back to (height, width)
+        perm = torch.LongTensor([1, 0])
+        keypoints = keypoints_xy_rotated[:, :, perm]
+
         return {'keypoints': keypoints, 'prob_map': prob_map}
 
 
@@ -110,7 +143,7 @@ class Encoder(nn.Module):
     def forward(self, input_dict: dict, need_masked_img: bool=False) -> dict:
         mask_batch = self.detector(input_dict)
         if need_masked_img:
-            damage_mask = torch.zeros(input_dict['img'].shape[0], 1, self.block, self.block, device=input_dict['img'].device).uniform_() > self.missing
-            damage_mask = F.interpolate(damage_mask.to(input_dict['img']), size=input_dict['img'].shape[-1], mode='nearest')
-            mask_batch['damaged_img'] = input_dict['img'] * damage_mask
+            damage_mask = torch.zeros(input_dict['img_rotated'].shape[0], 1, self.block, self.block, device=input_dict['img_rotated'].device).uniform_() > self.missing
+            damage_mask = F.interpolate(damage_mask.to(input_dict['img_rotated']), size=input_dict['img_rotated'].shape[-1], mode='nearest')
+            mask_batch['damaged_img'] = input_dict['img_rotated'] * damage_mask
         return mask_batch
